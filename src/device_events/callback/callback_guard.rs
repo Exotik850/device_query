16378c34acab4960e2652fdb9d424f8421ead7ac
Exotik850 <kidkool850@gmail.com>
2024-11-04 15:26:51 -0600

    Update CallbackGuard to support unsized callbacks

--- src/device_events/callback/callback_guard.rs
+++ src/device_events/callback/callback_guard.rs
@@ -5,6 +5,6 @@ use std::sync::Arc;
 /// Callback guard returned when adding a callback as an event listener. If the guard is dropped,
 /// the event listener is removed.
 #[derive(Debug)]
-pub struct CallbackGuard<Callback> {
+pub struct CallbackGuard<Callback: ?Sized> {
     pub(crate) _callback: Arc<Callback>,
 }
