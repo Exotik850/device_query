a0f19947cc2548f3ec48d2979baf0941a59f8762
kidkool850@gmail.com <kidkool850@gmail.com>
2024-11-04 19:17:25 -0600

    update examples

--- examples/event_based_print_keys.rs
+++ examples/event_based_print_keys.rs
@@ -1,11 +1,12 @@
 extern crate device_query;
 
-use device_query::{DeviceEvents, DeviceState};
+use device_query::{DeviceEvents, DeviceEventsHandler};
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let device_state = DeviceState::new();
+    let device_state =
+        DeviceEventsHandler::new(Duration::from_millis(10)).expect("Couldn't start event loop");
     let _guard = device_state.on_key_down(|key| {
         println!("Down: {:#?}", key);
     });
